2071d347d6e332327f032284bad912934a4d111f
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-13 12:37:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,6 @@
 def my_collect(array) 
   i = 0 
   collection = 0 
- my_collect(array) do |name|
-  name.split(" ").first
-  
   while i < array.length 
     collection << yield(array[i])
     i += 1 
