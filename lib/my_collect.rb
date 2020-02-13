1a76584fe9a0aa1bb579df89f70e61f5f3d640ea
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-13 12:36:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array) 
+  i = 0 
+  collection = 0 
+ my_collect(array) do |name|
+  name.split(" ").first
+  
+  while i < array.length 
+    collection << yield(array[i])
+    i += 1 
+    end
+  end
+  collection
+end
 
