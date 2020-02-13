17a59eef75800066c53dcc260bfe7e45ec2d41e7
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-13 12:44:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
-# def my_collect(array) 
-#   i = 0 
-#   collection = 0 
-#   while i < array.length 
-#     collection << yield(array[i])
-#     i += 1 
-#     end
-#   end
-#   collection
-# end
+def my_collect(array) 
+  i = 0 
+  collection = 0 
+  while i < array.length 
+    collection << yield(array[i])
+    i += 1 
+    end
+  end
+  collection
+end
 
 def my_collect(array) 
   new_arr = []
