fb6af7872071dd6c1dfaefd35532e21d91d08c30
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-13 12:43:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,18 @@
-def my_collect(array) 
-  i = 0 
-  collection = 0 
-  while i < array.length 
-    collection << yield(array[i])
-    i += 1 
-    end
-  end
-  collection
-end
+# def my_collect(array) 
+#   i = 0 
+#   collection = 0 
+#   while i < array.length 
+#     collection << yield(array[i])
+#     i += 1 
+#     end
+#   end
+#   collection
+# end
 
-if my_collect(array)
+def my_collect(array) 
+  new_arr = []
+  array.collect do |name| 
+    new_arr << name.upcase 
+    end 
+  new_arr
+end 
